05c7b5d2b9e6432b278f9b3efa3018d5ed23f434
matthias.lantsch <matthias.lantsch@bluewin.ch>
2014-09-30 14:04:39 +0200

    Neui activity, jetzt mues nuer no camera ine

--- Groessenmesser/src/ch/ez_gaming/groessenmesser/MeasureActivity.java
+++ Groessenmesser/src/ch/ez_gaming/groessenmesser/MeasureActivity.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MeasureActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//<--------Fullscreen stizzle
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //until here ----->
 		setContentView(R.layout.activity_measure);
 	}
 
